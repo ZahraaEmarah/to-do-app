c6377caf7731c9bc110f744e5a224ed1a4a65c56
ZahraaEmarah <zahraa.emara@syngenta.com>
2023-11-08 13:59:49 +0200

    error handling

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -5,10 +5,20 @@ import { AddForm } from './AddForm'
 import { TasksList } from './TasksList';
 import { getTasks, deleteTask, addTask, editTask } from '../api/TaskRouter'
 import { Loader } from './Loader';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const Wrapper = () => {
     const [tasks, setTasks] = useState([]);
     const [isLoading, setLoader] = useState(false);
+
+    const showErrorMessage = () => toast.error("Something went wrong!", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        theme: "colored"
+    });
 
     useEffect(() => {
         setLoader(true);
@@ -17,32 +27,50 @@ export const Wrapper = () => {
             setTasks(response.data.todos);
             setLoader(false);
         }
-        fetchData();
+        try {
+            fetchData();
+            throw new Error()
+        } catch (error) {
+            showErrorMessage();
+        }
     }, []);
 
     const handleAdd = async (task) => {
         setLoader(true);
-        const response = await addTask(task);
-        setTasks([...tasks, response.data]);
+        try {
+            const response = await addTask(task);
+            setTasks([...tasks, response.data]);
+        } catch (error) {
+            showErrorMessage();
+        }
         setLoader(false);
     }
 
     const handleEdit = async (id, task, isCompleted, userId) => {
-        const response = await editTask(id, task, isCompleted, userId);
-        setTasks(tasks.map(t => {
-            return t.id === id ? response.data : t;
-        }))
+        try {
+            const response = await editTask(id, task, isCompleted, userId);
+            setTasks(tasks.map(t => {
+                return t.id === id ? response.data : t;
+            }))
+        } catch (error) {
+            showErrorMessage();
+        }
     }
 
     const handleDelete = async (id) => {
-        const response = await deleteTask(id);
-        setTasks(tasks.map(t => {
-            return t.id === id ? response.data : t;
-        }))
+        try {
+            const response = await deleteTask(id);
+            setTasks(tasks.map(t => {
+                return t.id === id ? response.data : t;
+            }))
+        } catch (error) {
+            showErrorMessage();
+        }
     }
 
     return (
         <section className="vh-100">
+            <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick theme="colored" />
             <div className="container py-5 h-100">
                 <div className="row d-flex justify-content-center align-items-center h-100">
                     <div className="col">
@@ -54,7 +82,7 @@ export const Wrapper = () => {
                                 </p>
                                 <AddForm addTask={handleAdd} />
                                 <hr className="my-4"></hr>
-                                {   isLoading? <Loader />: ""   }
+                                {isLoading ? <Loader /> : ""}
                                 <TasksList handleEdit={handleEdit} handleDelete={handleDelete} tasks={tasks} />
                             </div>
                         </div>
